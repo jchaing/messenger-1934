4b57eaec789cc16a799abbbf2cde28e2a98b14cc
John Chaing <john@spinningplates.org>
2021-11-26 19:50:13 -0800

    refactored: assign copy of convo to new variable newConvo

--- client/src/store/utils/reducerFunctions.js
+++ client/src/store/utils/reducerFunctions.js
@@ -12,11 +12,11 @@ export const addMessageToStore = (state, payload) => {
   }
 
   return state.map((convo) => {
-    convo = {...convo}
-    if (convo.id === message.conversationId) {
-      convo.messages.push(message);
-      convo.latestMessageText = message.text;
-      return convo;
+    const newConvo = {...convo}
+    if (newConvo.id === message.conversationId) {
+      newConvo.messages.push(message);
+      newConvo.latestMessageText = message.text;
+      return newConvo;
     } else {
       return convo;
     }
@@ -69,12 +69,12 @@ export const addSearchedUsersToStore = (state, users) => {
 
 export const addNewConvoToStore = (state, recipientId, message) => {
   return state.map((convo) => {
-    convo = {...convo}
-    if (convo.otherUser.id === recipientId) {
-      convo.id = message.conversationId;
-      convo.messages.push(message);
-      convo.latestMessageText = message.text;
-      return convo;
+    const newConvo = {...convo}
+    if (newConvo.otherUser.id === recipientId) {
+      newConvo.id = message.conversationId;
+      newConvo.messages.push(message);
+      newConvo.latestMessageText = message.text;
+      return newConvo;
     } else {
       return convo;
     }

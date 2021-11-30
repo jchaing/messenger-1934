a9d86366a2c7bafb4143978e90d4b12f591b5d94
John Chaing <john@spinningplates.org>
2021-11-29 19:20:25 -0800

    styling for Login component page

--- client/src/Login.js
+++ client/src/Login.js
@@ -20,6 +20,190 @@ const useStyles = makeStyles(() => ({
     height: "700px",
     background: "#FFFFFF",
   },
+  createAccount: {
+    position: "absolute",
+    width: "351px",
+    height: "54px",
+    left: "631px",
+    top: "30px",
+  },
+  noAccount: {
+    position: "absolute",
+    height: "19px",
+    width: "152px",
+    left: "0%",
+    right: "56.98%",
+    top: "calc(50% - 19px/2 - 1.18px)",
+    fontFamily: "Open Sans",
+    fontStyle: "normal",
+    fontWeight: "normal",
+    fontSize: "14px",
+    lineHeight: "19px",
+    textAlign: "center",
+    color: "#B0B0B0",
+  },
+  buttonBox: {
+    position: "absolute",
+    left: "51.57%",
+    right: "0%",
+    top: "0%",
+    bottom: "0%",
+  },
+  createAccountButton: {
+    position: "absolute",
+    left: "0%",
+    right: "0%",
+    top: "0%",
+    bottom: "0%",
+    background: "#FFFFFF",
+    borderRadius: "5px",
+    fontFamily: "Open Sans",
+    fontStyle: "normal",
+    fontWeight: 600,
+    fontSize: "14px",
+    lineHeight: "19px",
+    textAlgin: "center",
+    color: "#3A8DFF",
+    paddingLeft: "34px",
+    paddingRight: "33px",
+    paddingTop: "16px",
+    paddingBottom: "19px",
+    "&:hover": {
+      boxShadow: "0px 2px 12px rgba(74, 106, 149, 0.2)",
+    },
+  },
+  form: {
+    position: "absolute",
+    width: "380px",
+    height: "358px",
+    left: "522px",
+    top: "170px",
+  },
+  welcomeBack: {
+    position: "absolute",
+    height: "40px",
+    left: "0%",
+    right: "50.26%",
+    top: "calc(50% - 40px/2 - 159px)",
+    fontFamily: "Open Sans",
+    fontStyle: "normal",
+    fontWeight: "600",
+    fontSize: "26px",
+    lineHeight: "40px",
+    /* identical to box height, or 154% */
+    color: "#000000",
+  },
+  usernameGrid: {
+    position: "absolute",
+    left: "0%",
+    right: "0%",
+    top: "20.39%",
+    bottom: "61.17%",
+  },
+  usernameLabel: {
+    "&.shrink": {
+      position: "absolute",
+      height: "19px",
+      left: "1.32%",
+      right: "73.42%",
+      top: "calc(50% - 19px/2 - 23.5px)",
+      fontFamily: "Open Sans",
+      fontStyle: "normal",
+      fontWeight: "normal",
+      fontSize: "19px",
+      lineHeight: "19px",
+      /* identical to box height */
+      color: "#B0B0B0",
+    },
+  },
+  inputText: {
+    height: "19px",
+    paddingLeft: "1.32%",
+    paddingTop: "20.18px",
+    right: "61.05%",
+    fontFamily: "Open Sans",
+    fontStyle: "normal",
+    fontWeight: "600",
+    fontSize: "14px",
+    lineHeight: "19px",
+    /* identical to box height */
+    color: "#000000",
+  },
+  passwordGrid: {
+    position: "absolute",
+    left: "0%",
+    right: "0%",
+    top: "49.44%",
+    bottom: "32.4%",
+    // zIndex: 1
+  },
+  passwordLabel: {
+    "&.shrink": {
+      position: "absolute",
+      height: "19px",
+      left: "1.32%",
+      right: "81.84%",
+      top: "calc(50% - 19px/2 - 23px)",
+      fontFamily: "Open Sans",
+      fontStyle: "normal",
+      fontWeight: "normal",
+      fontSize: "19px",
+      lineHeight: "19px",
+      /* identical to box height */
+      color: "#B0B0B0",
+    },
+  },
+  passwordText: {
+    height: "19px",
+    paddingLeft: "1.32%",
+    paddingTop: "20.18px",
+    right: "61.05%",
+    fontFamily: "Open Sans",
+    fontStyle: "normal",
+    fontWeight: "600",
+    fontSize: "20px",
+    lineHeight: "19px",
+    /* identical to box height */
+    color: "#000000",
+  },
+  forgotButton: {
+    position: "relative",
+    left: "0%",
+    right: "0%",
+    top: "-26px",
+    float: "right",
+    bottom: "0%",
+    background: "#FFFFFF",
+    borderRadius: "5px",
+    fontFamily: "Open Sans",
+    fontStyle: "normal",
+    fontWeight: 600,
+    fontSize: "12px",
+    lineHeight: "16px",
+    textAlgin: "center",
+    color: "#3A8DFF",
+    padding: 0,
+  },
+  loginButtonGrid: {
+    position: "absolute",
+    left: "28.95%",
+    right: "28.95%",
+    top: "84.36%",
+    bottom: "0%",
+  },
+  loginButton: {
+    position: "absolute",
+    top: "0%",
+    left: "0%",
+    right: "0%",
+    bottom: "0%",
+    borderRadius: "3px",
+    fontFamily: "Open Sans",
+    fontStyle: "normal",
+    fontWeight: "bold",
+    fontSize: "16px",
+    lineHeight: "22px",
+  },
 }));
 
 const Login = (props) => {
@@ -44,32 +228,79 @@ const Login = (props) => {
     <SideBanner />
     <Grid container justify="center">
       <Box>
-        <Grid container item>
-          <Typography>Need to register?</Typography>
-          <Button onClick={() => history.push("/register")}>Register</Button>
+        <Grid container className={classes.createAccount}>
+          <Typography className={classes.noAccount}>
+            Don't have an account?
+          </Typography>
+          <Grid className={classes.buttonBox}>
+            <Button
+              className={classes.createAccountButton}
+              onClick={() => history.push("/register")}
+            >
+              Create account
+            </Button>
+          </Grid>
         </Grid>
         <form onSubmit={handleLogin}>
-          <Grid>
-            <Grid>
-              <FormControl margin="normal" required>
+          <Grid className={classes.form}>
+            <Typography className={classes.welcomeBack}>
+              Welcome back!
+            </Typography>
+            <Grid className={classes.usernameGrid}>
+              <FormControl margin="normal" fullWidth="true" required>
                 <TextField
                   aria-label="username"
                   label="Username"
                   name="username"
                   type="text"
+                  InputProps={{
+                    classes: {
+                      input: classes.inputText
+                    }
+                  }}
+                  InputLabelProps={{
+                    classes: {
+                      root: classes.usernameLabel,
+                      shrink: "shrink"
+                    }
+                  }}
                 />
               </FormControl>
             </Grid>
-            <FormControl margin="normal" required>
-              <TextField
-                label="password"
-                aria-label="password"
-                type="password"
-                name="password"
-              />
-            </FormControl>
-            <Grid>
-              <Button type="submit" variant="contained" size="large">
+            <Grid className={classes.passwordGrid}>
+              <FormControl margin="normal" fullWidth="true" required>
+              <Grid>
+                <TextField
+                  label="Password"
+                  aria-label="password"
+                  type="password"
+                  name="password"
+                  InputProps={{
+                    classes: {
+                      input: classes.passwordText
+                    }
+                  }}
+                  InputLabelProps={{
+                    classes: {
+                      root: classes.passwordLabel,
+                      shrink: "shrink"
+                    }
+                  }}
+                  fullWidth="true"
+                />
+                <Button className={classes.forgotButton}>Forgot?</Button>
+              </Grid>
+              </FormControl>
+            </Grid>
+            <Grid className={classes.loginButtonGrid}>
+              <Button 
+                color="primary"
+                className={classes.loginButton}
+                type="submit"
+                variant="contained"
+                size="large"
+                fullWidth="true"
+              >
                 Login
               </Button>
             </Grid>
